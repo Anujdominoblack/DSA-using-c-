f8f84d8246f6cda364ed9806d811e72669e868cb
anuj <anujkumarpandey2727@gmail.com>
2023-07-09 14:07:55 +0530

    remove duplicates from a sorted array

---
+++ ARRAY/removedduplicates.cpp
@@ -0,0 +1,30 @@
+//idea create a  temporary array to store the elements then copy it
+#include<iostream>
+using  namespace std;
+int revdups(int arr[],int n)
+{
+    int temp[7];
+    int res=1;
+    for(int i=1;i<n;i++)
+    {
+        if(temp[res-1]!=arr[i])  //checking if different elements are present
+        {
+            temp[res]=arr[i];
+            res++;
+        }
+    }
+    for(int i=0;i<n;i++)  //copying the elements
+    {
+        arr[i]=temp[i];  
+    }
+    return res;  //returning the effective sizeof the array
+}
+
+int main()
+{
+    int arr[]={10,20,20,30,30,30,30};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int result=revdups(arr,n);
+    cout<<result;
+    return 0;
+}
