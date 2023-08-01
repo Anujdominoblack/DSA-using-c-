4ef149f6227bd6f28292cfb24c9c3fc5bbce619c
anuj <anujkumarpandey2727@gmail.com>
2023-08-01 22:33:45 +0530

    count occurences in 1's

---
+++ searching/count1insortedarray.cpp
@@ -0,0 +1,21 @@
+//count 1's naive solution 
+#include<iostream>
+using namespace std;
+
+int main()  //naive solution
+{
+    int arr[]={0,0,0,1,1,1};
+    int n=sizeof(arr)/sizeof(arr[0]);  //counting size and number of elements
+    int i;
+    for( i=0;i<n;i++)
+    {
+        if(arr[i]==1)
+        {
+            break;
+        }
+    }
+
+    cout<<(n-i);  //subtracting first index of i from size 
+    return 0;
+
+}
