ddbd6094f4149943005773af89803d06c62a1111
anuj <anujkumarpandey2727@gmail.com>
2023-08-09 22:35:59 +0530

    bit magic left

---
+++ bit magic/rightshift.cpp
@@ -0,0 +1,14 @@
+// right shift operation 
+#include<iostream>
+using namespace std;
+
+//driver code
+
+int main()
+{
+    int x;   //variable is created
+    cin>>x;
+    cout<<(x>>1)<<endl;  //output 
+    cout<<(x>>2)<<endl;  //output
+    return 0;
+}
