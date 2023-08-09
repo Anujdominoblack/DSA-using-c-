20f385a6007ce3d07f3dd7d3549499ea535a7b2b
anuj <anujkumarpandey2727@gmail.com>
2023-08-09 22:57:24 +0530

    bitwise not

---
+++ bit magic/bitwisenot.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+//driver code
+
+int main()
+{
+    unsigned int x=1;  //variable is created
+    cout<<(~x)<<endl;  //output 
+    x=5;
+    cout<<(~x)<<endl;  //output
+    return 0;
+}
