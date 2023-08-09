0e1c0e249872c3995de8123712498ff5047b0ec9
anuj <anujkumarpandey2727@gmail.com>
2023-08-09 23:01:46 +0530

    bitwise not 1

---
+++ bit magic/bitwisenot1.cpp
@@ -0,0 +1,14 @@
+//bitwise not
+#include<iostream>
+using namespace std;
+
+//driver code
+
+int main()
+{
+    int x=1;
+    cout<<"enter the value whose bitwise you want to find"<<endl;
+    cin>>x;
+    cout<<(~x)<<endl;
+    return 0;
+}
