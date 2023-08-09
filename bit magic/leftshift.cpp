15573c6d95b1590239bf3efac4dc18b8f71edfc0
anuj <anujkumarpandey2727@gmail.com>
2023-08-09 22:26:24 +0530

    bit magic

---
+++ bit magic/leftshift.cpp
@@ -0,0 +1,13 @@
+//we will do how left shift works 
+#include<iostream>
+using namespace std;
+
+//driver code
+int main()
+{
+    int x;
+    cin>>x;
+    cout<<(x<<1)<<endl;  //left shift increases the value
+    cout<<(x<<2)<<endl;
+    return 0;
+}
