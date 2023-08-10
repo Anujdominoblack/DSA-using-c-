990aba37d375e915f1749e11993559049c91ac0c
anuj <anujkumarpandey2727@gmail.com>
2023-08-10 21:47:56 +0530

    left shift example 2

---
+++ bit magic/leftshift2.cpp
@@ -0,0 +1,17 @@
+//in this we will use two values and find a left shift 
+#include<iostream>
+using namespace std;
+
+//driver code
+
+int main()
+{
+    int x;
+    int y;
+    int z;
+    cout<<"enter the values of x and y";
+    cin>>x>>y;
+    z=x<<y;
+    cout<<z;
+    return 0;
+}
