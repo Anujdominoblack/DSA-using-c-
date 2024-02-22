15208fc89caade2938337f2b62ddea39146c4925
anuj <anujkumarpandey2727@gmail.com>
2024-02-22 22:57:09 +0530

    iterating through string

---
+++ strings/iteratingthroughstring.cpp
@@ -0,0 +1,13 @@
+// in this example we will see how to iter through strings
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    string str = "thisisgithub";
+    for(int i=0;i<str.length();i++)
+    {
+        cout<<str[i]<<" ";
+    }
+    return 0;
+}
