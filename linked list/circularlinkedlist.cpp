497d4ef4e5fd5e779d385a8cbbf634713ab75ea4
anuj <anujkumarpandey2727@gmail.com>
2024-03-09 23:57:16 +0530

    circular linked list intro

---
+++ linked list/circularlinkedlist.cpp
@@ -0,0 +1,39 @@
+//in circular linked list next of last node is head node
+#include<iostream>
+using namespace std;
+
+struct node
+{
+    int data;
+    node *next;
+    node(int d)
+    {
+        data =d;
+        next = NULL;
+    }
+};
+
+void printlist(node *head)
+{
+    if(head ==NULL)
+    {
+        return;
+    }
+    cout<<(head->data)<<" "; //printing head node
+    for(node *p = head->next ; p!=head ;p=p->next) //writing condition
+    {
+        cout<<(p->data)<<" ";
+    }
+}
+
+//driver code
+int main()
+{
+    node *head = new node(20);
+    head->next = new node(30);
+    head->next->next = new node(40);
+    head->next->next->next = new node(50);
+    head->next->next->next->next = head; 
+    printlist(head); //linking of circular linked list 
+    return 0;
+}
